7924da582995d69d4f5cada32b53d9c5ac9e4e68
Jules Fouchy <jules.fouchy@ntymail.com>
2020-04-30 00:14:22 +0200

    Dont log action 'checkDragging' to prevent spam

--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@ import actions from './actions'
 
 import './main.scss'
 
-withLogger(app)(
+withLogger({
+    log(prevState, action, nextState) {
+      if (action.name != 'checkDragging') {
+            console.group("%c action", "color: gray; font-weight: lighter;", action.name)
+            //console.log("%c prev state", "color: #9E9E9E; font-weight: bold;", prevState)
+            console.log("%c data", "color: #03A9F4; font-weight: bold;", action.data)
+            console.log("%c next state", "color: #4CAF50; font-weight: bold;", nextState)
+            console.groupEnd()
+      }
+    }
+  })(app)(
     state,
     actions,
     view,
